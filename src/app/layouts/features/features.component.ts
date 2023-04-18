8cc773d2907678f1c1e9551cc67006b42e23583a
shahil <shahilcode@gmail.com>
2023-04-18 16:35:06 +0530

    added the animation for the feature section

--- src/app/layouts/features/features.component.ts
+++ src/app/layouts/features/features.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
+
 
 @Component({
   selector: 'app-features',
   templateUrl: './features.component.html',
   styleUrls: ['./features.component.scss']
 })
-export class FeaturesComponent {
+export class FeaturesComponent implements OnInit {
+
+  ngOnInit(): void {
+    AOS.init();
+  }
 
 }
